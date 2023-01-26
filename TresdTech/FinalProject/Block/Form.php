af47f478dffbbecb2c22ca2bbce9e8217e5d7151
Antonio Balcazar <antonio_vendeta@hotmail.com>
2023-01-26 01:43:20 +0000

    empezando el observador

---
+++ TresdTech/FinalProject/Block/Form.php
@@ -0,0 +1,12 @@
+<?php
+namespace TresdTech\FinalProject\Block;
+
+class Form extends \Magento\Framework\View\Element\Template
+{
+	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	{
+		parent::__construct($context);
+	}
+
+
+}
---
+++ TresdTech/FinalProject/Observer/Car.php
@@ -0,0 +1,24 @@
+<?php
+
+// namespace TresdTech\FinalProject\Observer;
+
+// use Magento\Framework\Event\ObserverInterface;
+
+// class Car implements ObserverInterface{
+
+//     public function __construct (\Psr\Log\LoggerInterface $logger){
+//         $this->logger = $logger;
+//         }
+
+
+
+//         public function execute (\Magento\Framework\Event\Observer $observer){
+
+//             $qty = $quote->getItemsSummaryQty();
+            
+//             if($qty >= 6){
+            
+//             throw new \Magento\Framework\Exception\NoSuchEntityException(_('Ya no puedes agregar mas'));
+//             }
+//         }
+// }
